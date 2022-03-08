ef9119f336f562c509fee1bec95c3cd47dd2bf16
Tamjid Miah <tm620@ic.ac.uk>
2022-03-08 23:31:11 +0000

    Added a comment

--- Models/Decision_Tree.py
+++ Models/Decision_Tree.py
@@ -19,6 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 tree = DecisionTreeClassifier(random_state=0)
 tree.fit(X_train,y_train)
 
+#Visualisation of model
 fig = plt.figure(figsize=(25,20))
 _ = plot_tree(tree, 
                    feature_names=['AGE','Urea','Cr','HbA1c','Chol','TG','HDL','LDL','VLDL','BMI','Male','Female'],  
