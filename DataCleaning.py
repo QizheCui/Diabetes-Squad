8fe259b75ef98449b54915f8dabfb2136bbfac16
leijohansson <linnealeijohansson@outlook.com>
2022-03-09 11:19:20 +0000

    Data splitting
    Inserted code to split data into testing and training data into DataCleaning.py

--- DataCleaning.py
+++ DataCleaning.py
@@ -9,14 +9,49 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as optimize
-data = pd.read_csv('diabetes.txt', delimiter = '\t')
+import random
 
-sex1 = data.query('SEX == 1')
-sex2 = data.query('SEX == 2')
+data = pd.read_csv('.\EDA\Mendeley.csv')
+fraction = 0.7 #fraction of data used for testing
 
-bmi_mean1 = np.mean(sex1['BMI'])
-print(bmi_mean1)
-bmi_mean2 = np.mean(sex2['BMI'])
-print(bmi_mean2)
+del data['No_Pation'] #deleting patient numner
+del data['ID'] #deleting ID number
 
-#2 might be male as the mean BMI is larger
+#setting gender as boolean (2 columns)
+data['Female'] = (data['Gender']=='F').astype(int) 
+data['Male'] = (data['Gender']=='M').astype(int)
+del data['Gender']
+
+#centering columns at 0
+for i in data.columns[:-3]:
+    data[i]=data[i]-data[i].mean()
+
+#rearranging columns so that the diabetes diagnosis is the last column
+column_names=list(data.columns.values)
+column_names[-1], column_names[-3] = column_names[-3], column_names[-1]
+data = data.reindex(columns=column_names)
+
+#saving the file
+data.to_csv('.\EDA\Mendeley_normalised.csv')
+
+#Generating the random index for the sample
+len_training = int(len(data)*fraction)
+index = random.sample(range(len(data)), len_training)
+
+#selecting training data using the index
+training_data = data.iloc[index]
+#saving the data
+training_data.to_csv('.\EDA\Mendeley_training'+str(fraction)+'.csv')
+
+#taking all the indexes not used in training data
+test_index =[]
+for i in range(len(data)):
+    if i in index == False:
+        test_index.append(i)
+
+#selecting the test data
+testing_data = data.iloc(test_index)
+#saving the test data
+training_data.to_csv('.\EDA\Mendeley_testing'+str(fraction)+'.csv')
+
+        
---
+++ Logistics_regression.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 10:51:14 2022
+
+@author: Linne
+"""
+
+from sklearn.linear_model import LogisticRegression
+logisticRegr = LogisticRegression()
