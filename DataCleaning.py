1d7dbecd12e829f0e8904777664a29772efd6537
leijohansson <linnealeijohansson@outlook.com>
2022-03-15 18:44:52 +0000

    Data Cleaning, scaled data
    removed class = 'P' from data, scaled the data (standard scaler)

--- DataCleaning.py
+++ DataCleaning.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as optimize
 import random
+from sklearn import preprocessing
 
 data = pd.read_csv('.\EDA\Mendeley.csv')
 fraction = 0.7 #fraction of data used for testing
@@ -21,6 +22,11 @@ del data['ID'] #deleting ID number
 data['Female'] = (data['Gender']=='F').astype(int) 
 data['Male'] = (data['Gender']=='M').astype(int)
 del data['Gender']
+#removing predict diabetes data, diabetes diagnosis as 1 or 0
+data['CLASS'] = data['CLASS'].replace(['P'],np.nan)
+data.dropna(axis=1)
+data['CLASS'] = (data['CLASS']=='Y').astype(int)
+        
 
 #centering columns at 0
 for i in data.columns[:-3]:
@@ -31,8 +37,15 @@ column_names=list(data.columns.values)
 column_names[-1], column_names[-3] = column_names[-3], column_names[-1]
 data = data.reindex(columns=column_names)
 
+
 #saving the file
 data.to_csv('.\EDA\Mendeley_normalised.csv')
+
+#scaling
+scaler = preprocessing.StandardScaler().fit(data[data.columns[:-1]])
+X_scaled= scaler.transform(data[data.columns[:-1]])
+data_scaled= np.concatenate((X_scaled,data['CLASS'].to_numpy().reshape(len(data),1)),1)
+data_scaled = pd.DataFrame(data_scaled, columns = data.columns)
 
 #Generating the random index for the sample
 len_training = int(len(data)*fraction)
@@ -40,8 +53,10 @@ index = random.sample(range(len(data)), len_training)
 
 #selecting training data using the index
 training_data = data.iloc[index]
+training_data_scaled = data_scaled.iloc[index]
 #saving the data
-training_data.to_csv('.\EDA\Mendeley_training'+str(fraction)+'.csv')
+training_data.to_csv('.\EDA\Mendeley_training'+str(int(fraction*100))+'.csv')
+training_data_scaled.to_csv('.\EDA\Mendeley_training'+str(int(fraction*100))+'_ss.csv')
 
 #taking all the indexes not used in training data
 test_index =[]
@@ -50,8 +65,9 @@ for i in range(len(data)):
         test_index.append(i)
 
 #selecting the test data
-testing_data = data.iloc(test_index)
+testing_data = data.iloc[test_index]
+testing_data_scaled = data_scaled.iloc[test_index]
 #saving the test data
-training_data.to_csv('.\EDA\Mendeley_testing'+str(fraction)+'.csv')
-
+testing_data.to_csv('.\EDA\Mendeley_testing'+str(int(fraction*100))+'.csv')
+testing_data_scaled.to_csv('.\EDA\Mendeley_testing'+str(int(fraction*100))+'_ss.csv')
         
--- Logistics_regression.py
+++ Logistics_regression.py
@@ -4,6 +4,18 @@ Created on Wed Mar  9 10:51:14 2022
 
 @author: Linne
 """
-
 from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as optimize
+
 logisticRegr = LogisticRegression()
+data = pd.read_csv('.\EDA\Mendeley_training70.csv')
+columns = data.columns
+x_train = data[data.columns[:-1]]
+t_train = data[data.columns[-1]]
+
+logisticRegr.fit(x_train, y_train)
+
+testdata = pd.read_csv('.\EDA\Mendeley_testing70.csv')
